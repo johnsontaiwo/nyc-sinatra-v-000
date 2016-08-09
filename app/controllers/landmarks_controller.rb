bcc9d617d60a25de7008936d35251a021de253f1
johnsontaiwo <josseytee@gmail.com>
2016-08-09 10:50:00 -0400

    modified files

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -15,8 +15,9 @@ class LandmarksController < ApplicationController
   
   get '/landmarks/:id' do
     @landmark = Landmark.find_by_id(params[:id])
-    erb :'/landmarks/single_show'
+    erb :'/landmarks/show'
   end
+  
   get '/landmarks/:id/edit' do
     @landmark = Landmark.find_by_id(params[:id])
     erb :'/landmarks/edit'
